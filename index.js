6800178c70497ff501eeb524b94b1b51e10f79f3
Siddharth Singh <114818090+Siddharth-Singh-28@users.noreply.github.com>
2024-11-21 15:42:40 +0530

    Add files via upload
    Added index,html, index.js and style,css

---
+++ index.js
@@ -0,0 +1,19 @@
+let result = "";
+let buttons = document.querySelectorAll("button");
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if(e.target.innerHTML == "="){
+            result = eval(result);
+            document.querySelector('input').value = result;
+        }
+        else if(e.target.innerHTML == "C"){
+            result = "";
+            document.querySelector('input').value = result;
+        }
+        else{
+            console.log(e.target)
+            result += e.target.innerHTML;
+            document.querySelector('input').value = result;
+        }
+    })
+})
